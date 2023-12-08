82e3612ccabd7eeeaf5ad2aca82e99835e4d82d4
ltpoole <laborris.poole@gmail.com>
2023-12-07 20:48:55 -0500

    Completed Event Handler tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
-import Header from './components/Header/Header';
-import MovieList from './components/MovieList/MovieList';
-import MovieInfo from './components/MovieInfo/MovieInfo';
+import "./App.css";
+import Header from "./components/Header/Header";
+import MovieList from "./components/MovieList/MovieList";
+import MovieInfo from "./components/MovieInfo/MovieInfo";
 
-function App() { 
+function App() {
   const selectedMovie = {
     title: "Forrest Gump",
-    runningTime: 142,
-    genre: "Romantic Drama"
-  }
+    runningTime: 136,
+    genre: "Romantic Drama",
+  };
 
   return (
     <div className="App">
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,16 @@
+import MovieItem from "../MovieItem/MovieItem";
+
 const MovieList = ({}) => {
-    const movies = ['Princess Bride', 'Forrest Gump', 'Iron Man', 'Pulp Fiction']
-    const movieItems = movies.map((movie) => <div>{movie}</div>)
-    return ( 
-        <div className="flex-item">
-            <h4>My Movies</h4>
-            <div>
-                {movieItems}
-            </div>
-        </div> 
-    );
-}
- 
+  const movies = ["Princess Bride", "Forrest Gump", "Iron Man", "Pulp Fiction"];
+  const movieItems = movies.map((movie) => (
+    <MovieItem key={movie} title={movie} />
+  ));
+  return (
+    <div className="flex-item">
+      <h4>My Movies</h4>
+      <div>{movieItems}</div>
+    </div>
+  );
+};
+
 export default MovieList;
